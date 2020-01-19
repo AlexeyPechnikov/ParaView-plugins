38772003754b0910759bbcffd6ccc556ec207130
Alexey Pechnikov <pechnikov@mobigroup.ru>
2020-01-20 01:32:44 +0700

    Add UI for new Table On Topography Block Source

--- NCube/NCubeParaViewPlugin.py
+++ NCube/NCubeParaViewPlugin.py
@@ -407,6 +407,259 @@ def _NCubeDataSetToGeoDataFrame(vtk_data):
 
 
 #------------------------------------------------------------------------------
+# N-Cube Table On Topography Block Source
+#
+# Create 3D points when X, Y Fields and maybe Z Field defined (overwise Z=0)
+# AND
+# Create 3D unit vector when Azimuth,Dip Fields defined and Length Field is not defined
+#
+# OR
+#
+# Create 3D lines when X,Y,Z,Azimuth,Dip,Length Fields defined
+#
+#------------------------------------------------------------------------------
+
+@smproxy.source(name="NCubeTableOnTopographyBlockSource",
+       label="N-Cube Table On Topography Block Source")
+class NCubeTableOnTopographyBlockSource(VTKPythonAlgorithmBase):
+    def __init__(self):
+        VTKPythonAlgorithmBase.__init__(self,
+                nInputPorts=0,
+                nOutputPorts=1,
+                outputType='vtkMultiBlockDataSet')
+        self._tablename = None
+        self._toponame = None
+        self._xcol = None
+        self._ycol = None
+        self._zcol = None
+        self._zunit = 'm'
+
+        self._azcol = None
+        self._dipcol = None
+        # if it's defined, create segment, overwise ?only create point with vector
+        self._lengthcol = None
+        self._lengthunit = 'm'
+        self._vectorname = 'vector'
+
+        #self._col = None
+        self._epsg = None
+
+
+    def RequestData(self, request, inInfo, outInfo):
+        from vtk import vtkPolyData, vtkAppendPolyData, vtkCompositeDataSet, vtkMultiBlockDataSet
+        import time
+
+        if self._tablename is None:
+            return 1
+
+        t0 = time.time()
+#        vtk_blocks = _NCubeGeometryOnTopography(self._shapename, self._toponame, self._shapecol, self._shapeencoding)
+#        if vtk_blocks is None or vtk_blocks == []:
+#            t1 = time.time()
+#            print ("t1-t0", t1-t0)
+#            return 1
+#        print ("vtk_blocks", len(vtk_blocks))
+#        mb = vtkMultiBlockDataSet.GetData(outInfo, 0)
+#        mb.SetNumberOfBlocks(len(vtk_blocks))
+#        rowidx = 0
+#        for (label, polyData) in vtk_blocks:
+#            #print (rowidx, label)
+#            mb.SetBlock( rowidx, polyData )
+#            mb.GetMetaData( rowidx ).Set( vtkCompositeDataSet.NAME(), label)
+#            rowidx += 1
+        t1 = time.time()
+        print ("t1-t0", t1-t0)
+
+        return 1
+
+    @smproperty.stringvector(name="Table File Name")
+    @smdomain.filelist()
+    @smhint.filechooser(extensions=["csv"], file_description="CSV")
+    def SetShapeFileName(self, name):
+        """Specify filename for the table to read."""
+        print ("SetCSVFileName", name)
+        name = name if name != 'None' else None
+        if self._tablename != name:
+            self._tablename = name
+            self.Modified()
+
+    @smproperty.stringvector(name="Topography File Name (optional)")
+    @smdomain.filelist()
+    @smhint.filechooser(extensions=["tif", "TIF", "nc"], file_description="GeoTIFF, NetCDF")
+    def SetTopographyFileName(self, name):
+        """Specify filename for the topography file to read."""
+        print ("SetTopographyFileName", name)
+        name = name if name != 'None' else None
+        if self._toponame != name:
+            self._toponame = name
+            self.Modified()
+
+    # Multiblock labels
+    @smproperty.stringvector(name="Columns", information_only="1")
+    def GetColumns(self):
+        if self._tablename is None:
+            return []
+        # Load file
+        import pandas as pd
+        df = pd.read_csv(self._tablename, nrows=0)
+        cols = sorted(df.columns.values)
+        return ['None'] + list(map(str,cols))
+    @smproperty.stringvector(name="Group by (optional)", number_of_elements="1")
+    @smdomain.xml(\
+        """<StringListDomain name="list">
+                <RequiredProperties>
+                    <Property name="Columns" function="GetColumns"/>
+                </RequiredProperties>
+            </StringListDomain>
+        """)
+    def SetLabel(self, col):
+        col = col if col != 'None' else None
+        self._tablecol = col
+        print("SetLabel", col)
+        self.Modified()
+
+    # possible coordinate columns
+    @smproperty.stringvector(name="NumericColumns", information_only="1")
+    def GetNumericColumns(self):
+        print ("GetColumns 0")
+        if self._tablename is None:
+            return []
+        # Load file
+        import pandas as pd
+#        df = pd.read_csv(self._tablename, low_memory=False)
+        df = pd.read_csv(self._tablename, nrows=1)
+        cols = sorted(df._get_numeric_data().columns)
+        print ("GetColumns", cols)
+        return ['None'] + list(map(str,cols))
+
+    @smproperty.stringvector(name="Easting", number_of_elements="1")
+    @smdomain.xml(\
+        """<StringListDomain name="list">
+                <RequiredProperties>
+                    <Property name="NumericColumns" function="GetNumericColumns"/>
+                </RequiredProperties>
+            </StringListDomain>
+        """)
+    def SetXColumn(self, col):
+        col = col if col != 'None' else None
+        self._xcol = col
+        print("SetXColumn", col)
+        self.Modified()
+
+    @smproperty.stringvector(name="Northing", number_of_elements="1")
+    @smdomain.xml(\
+        """<StringListDomain name="list">
+                <RequiredProperties>
+                    <Property name="NumericColumns" function="GetNumericColumns"/>
+                </RequiredProperties>
+            </StringListDomain>
+        """)
+    def SetYColumn(self, col):
+        col = col if col != 'None' else None
+        self._ycol = col
+        print("SetYColumn", col)
+        self.Modified()
+
+    @smproperty.stringvector(name="Elevation (optional)", number_of_elements="1")
+    @smdomain.xml(\
+        """<StringListDomain name="list">
+                <RequiredProperties>
+                    <Property name="NumericColumns" function="GetNumericColumns"/>
+                </RequiredProperties>
+            </StringListDomain>
+        """)
+    def SetZColumn(self, col):
+        col = col if col != 'None' else None
+        self._zcol = col
+        print("SetZColumn", col)
+        self.Modified()
+
+    @smproperty.stringvector(name="Azimuth (optional)", number_of_elements="1")
+    @smdomain.xml(\
+        """<StringListDomain name="list">
+                <RequiredProperties>
+                    <Property name="NumericColumns" function="GetNumericColumns"/>
+                </RequiredProperties>
+            </StringListDomain>
+        """)
+    def SetAzimuthColumn(self, col):
+        col = col if col != 'None' else None
+        self._azcol = col
+        print("SetAzColumn", col)
+        self.Modified()
+
+    @smproperty.stringvector(name="Dip (optional)", number_of_elements="1")
+    @smdomain.xml(\
+        """<StringListDomain name="list">
+                <RequiredProperties>
+                    <Property name="NumericColumns" function="GetNumericColumns"/>
+                </RequiredProperties>
+            </StringListDomain>
+        """)
+    def SetDipColumn(self, col):
+        col = col if col != 'None' else None
+        self._dipcol = col
+        print("SetDipColumn", col)
+        self.Modified()
+
+    @smproperty.stringvector(name="Length (optional)", number_of_elements="1")
+    @smdomain.xml(\
+        """<StringListDomain name="list">
+                <RequiredProperties>
+                    <Property name="NumericColumns" function="GetNumericColumns"/>
+                </RequiredProperties>
+            </StringListDomain>
+        """)
+    def SetLengthColumn(self, col):
+        col = col if col != 'None' else None
+        self._lengthcol = col
+        print("SetLengthColumn", col)
+        self.Modified()
+
+
+    @smproperty.stringvector(name="Units", information_only="1")
+    def GetUnits(self):
+        return ["m","km","ft","kft"]
+
+    @smproperty.stringvector(name="Length Unit", number_of_elements="1")
+    @smdomain.xml(\
+        """<StringListDomain name="list">
+                <RequiredProperties>
+                    <Property name="Units" function="GetUnits"/>
+                </RequiredProperties>
+            </StringListDomain>
+        """)
+    def SetLengthUnit(self, unit):
+        self._lengthunit = unit
+        print("SetLengthUnit", unit)
+        self.Modified()
+
+    @smproperty.stringvector(name="Z Unit", number_of_elements="1")
+    @smdomain.xml(\
+        """<StringListDomain name="list">
+                <RequiredProperties>
+                    <Property name="Units" function="GetUnits"/>
+                </RequiredProperties>
+            </StringListDomain>
+        """)
+    def SetZUnit(self, unit):
+        self._zunit = unit
+        print("SetZUnit", unit)
+        self.Modified()
+
+    @smproperty.intvector(name=" EPSG (optional)", default_values=0)
+    def SetEPSG(self, epsg):
+        self._epsg = epsg
+        self.Modified()
+
+    @smproperty.stringvector(name="Output Vector", default_values='vector')
+    def SetVectorName(self, name):
+        print ("SetVectorName", name)
+        self._vectorname = name
+        self.Modified()
+
+
+#------------------------------------------------------------------------------
 # N-Cube Shapefile On Topography Block Source
 #------------------------------------------------------------------------------
 
