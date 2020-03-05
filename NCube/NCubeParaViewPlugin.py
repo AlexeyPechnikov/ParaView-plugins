93266708ddc9451830c52a3440ea5a19915675b2
Alexey Pechnikov <pechnikov@mobigroup.ru>
2020-03-06 02:41:11 +0700

    Load reproject filter

--- NCube/NCubeParaViewPlugin.py
+++ NCube/NCubeParaViewPlugin.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.dirname(__file__))
 from NCubeGeometryOnTopographyBlockSource import *
 from NCubeImageOnTopographySource import *
 from NCubeLASReader import *
+from NCubeReprojectFilter import *
 from NCubeShapefileWriter import *
 from NCubeTableOnTopographyBlockSource import *
 from NCubeTopographyBlockSource import *
-
