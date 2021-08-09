de1c72cf4f1bcd21a32d0e71254ba896292e6c4b
Alexey Pechnikov <pechnikov@mobigroup.ru>
2021-08-09 16:02:43 +0700

    Reorginize the code. Add processing for 2D coordinates for image on raster plugin.

--- NCube/NCube.py
+++ NCube/NCube.py
@@ -310,3 +310,191 @@ def _NCubeGeoDataFrameRowToVTKArrays(items):
         vtk_row.append((vtk_arr, value))
     return vtk_row
 
+#------------------------------------------------------------------------------
+# N-Cube Topography Block Source
+#------------------------------------------------------------------------------
+def _NCubeTopographyToGrid(dem):
+    from vtk import vtkPoints, vtkStructuredGrid, vtkThreshold, vtkDataObject, VTK_FLOAT
+    from vtk.util import numpy_support as vn
+    import numpy as np
+    
+    #print (dem)
+
+    xs = dem.x.values
+    ys = dem.y.values
+    if dem.dims == ('y','x'):
+        values = dem.values
+    else:
+        values = dem.values.T
+
+    # create raster mask by geometry and for NaNs
+    #mask = values.ravel('C')==dem.nodatavals[0]
+    #print (mask)
+    (yy,xx) = np.meshgrid(ys, xs)
+    vtk_points = vtkPoints()
+    points = np.column_stack((xx.ravel('F'),yy.ravel('F'),values.ravel('C')))
+    _points = vn.numpy_to_vtk(points, deep=True)
+    vtk_points.SetData(_points)
+#    for (_x,_y,_z,_m) in zip(xx.ravel('F'),yy.ravel('F'),values.ravel('C'),mask.ravel('C')):
+#        vtk_points.InsertNextPoint(_x,_y, _z if _m else np.nan)
+
+    sgrid = vtkStructuredGrid()
+    sgrid.SetDimensions(len(xs), len(ys), 1)
+    sgrid.SetPoints(vtk_points)
+
+    # mask NODATA values
+    _values = values.ravel('C').astype(float)
+    _values[_values == dem.nodatavals[0]] = np.nan
+    #array = vn.numpy_to_vtk(values.ravel('C'), deep=True, array_type=VTK_FLOAT)
+    array = vn.numpy_to_vtk(_values, deep=True, array_type=VTK_FLOAT)
+    array.SetName("z")
+    sgrid.GetPointData().AddArray(array)
+
+    thresh = vtkThreshold()
+    thresh.SetInputData(sgrid)
+    thresh.SetInputArrayToProcess(0, 0, 0, vtkDataObject.FIELD_ASSOCIATION_POINTS, "z")
+    thresh.ThresholdBetween(-1e30, 1e30)
+    thresh.Update()
+
+#    return sgrid
+    return thresh.GetOutput()
+
+# process rasterio dem object plus geodataframe
+def _NCubeTopography(dem, df):
+    from vtk import vtkAppendFilter
+    #vtkPoints, vtkCellArray, vtkStringArray, vtkIntArray, vtkFloatArray, vtkBitArray
+    from shapely.geometry.base import BaseGeometry, BaseMultipartGeometry
+    from shapely.geometry import box
+    import xarray as xr
+    import numpy as np
+    import rasterio.mask
+
+    dem_crs = dem.crs.to_string() if dem.crs is not None else None
+    dem_extent = box(dem.bounds.left,dem.bounds.bottom,dem.bounds.right,dem.bounds.top)
+    df = _NCubeGeoDataFrameToTopography(df, dem_extent, dem_crs)
+
+    groups = df.index.unique()
+    #print ("groups",groups)
+
+    vtk_blocks = []
+    # iterate blocks
+    for group in groups:
+        #print ("group",group)
+        # Python 2 string issue wrapped
+#        if hasattr(group, 'encode'):
+#            geoms = df[df.index.str.match(group)].geometry
+#        else:
+        geoms = df[df.index == group].geometry
+
+        vtk_append = vtkAppendFilter()
+        # rasterize geomemetry
+        try:
+            # use only 1st band from the raster
+            out_image, out_transform = rasterio.mask.mask(dem, geoms, indexes=1, crop=True, filled=True)
+        except:
+            # geometry outside of the raster, etc.
+            print (group, "exception")
+            continue
+        (xs, _) = rasterio.transform.xy(out_transform, out_image.shape[1]*[0], range(out_image.shape[1]), offset='center')
+        (_, ys) = rasterio.transform.xy(out_transform, range(out_image.shape[0]), out_image.shape[0]*[0], offset='center')
+        da = xr.DataArray(out_image, coords={'x': xs, 'y': ys}, dims=('y', 'x'))
+        da.attrs['nodatavals'] = (dem.nodata,)
+        # process rasterized geometry
+        vtk_ugrid = _NCubeTopographyToGrid(da)
+        if vtk_ugrid is None:
+            continue
+        if df.index is not None:
+            vtk_arrays = _NCubeGeoDataFrameRowToVTKArrays({df.index.name:group})
+            for (vtk_arr, val) in vtk_arrays:
+                for _ in range(vtk_ugrid.GetNumberOfCells()):
+                    vtk_arr.InsertNextValue(val)
+                vtk_ugrid.GetCellData().AddArray(vtk_arr)
+        # compose
+        vtk_append.AddInputData(vtk_ugrid)
+        # nothing to process
+        if vtk_append.GetNumberOfInputConnections(0) == 0:
+            continue
+        vtk_append.Update()
+        vtk_block = vtk_append.GetOutput()
+        vtk_blocks.append((str(group),vtk_block))
+
+    print ("_NCubeTopography end")
+
+    return vtk_blocks
+
+#------------------------------------------------------------------------------
+# N-Cube Image On Topography Source
+#------------------------------------------------------------------------------
+def _NCubeImageOnTopographyToGrid(dem, image, mask_magic=False):
+    from vtk import vtkPoints, vtkStructuredGrid, vtkThreshold, vtkDataObject, VTK_FLOAT, VTK_UNSIGNED_CHAR
+    from vtk.util import numpy_support as vn
+    import numpy as np
+
+    # mask NaN areas
+    nanmask = (~np.any(np.isnan(image.values),axis=0)).astype(float)
+    # mask single channel zeroes if needed
+    if mask_magic:
+        # that's more correct way, only black pixels ignored
+        #zeromask = (~np.all(image.values==0,axis=0)).astype(float)
+        # that's magic for better borders
+        zeromask = (~np.any(image.values==0,axis=0)).astype(float)
+        mask = nanmask*zeromask
+    else:
+        mask = nanmask
+    mask[mask==0] = np.nan
+
+    xs = dem.x.values
+    ys = dem.y.values
+    values = mask * dem.values
+
+    # create raster mask by geometry and for NaNs
+    (yy,xx) = np.meshgrid(ys, xs)
+    vtk_points = vtkPoints()
+    points = np.column_stack((xx.ravel('F'),yy.ravel('F'),values.ravel('C')))
+    _points = vn.numpy_to_vtk(points, deep=True)
+    vtk_points.SetData(_points)
+
+    sgrid = vtkStructuredGrid()
+    sgrid.SetDimensions(len(xs), len(ys), 1)
+    sgrid.SetPoints(vtk_points)
+
+    # convert datatype to Float32
+    array = vn.numpy_to_vtk(values.astype(np.float32).ravel(), deep=True, array_type=VTK_FLOAT)
+    array.SetName("z")
+    sgrid.GetPointData().AddArray(array)
+
+    # iterate 2D only coordinates
+    for coord in dem.coords:
+        if len(dem.coords[coord].shape) != 2:
+            continue
+        print (len(dem.coords[coord].shape))
+        # convert datatype to Float32
+        array = vn.numpy_to_vtk(dem.coords[coord].values.astype(np.float32).ravel(), deep=True, array_type=VTK_FLOAT)
+        array.SetName(coord)
+        sgrid.GetPointData().AddArray(array)
+
+    bands = image.band.shape[0]
+    print ("bands", bands)
+    if bands == 3:
+        # RGB
+        colors = np.round(image.values)
+        array = vn.numpy_to_vtk(colors.reshape(3,-1).T, deep=True, array_type=VTK_UNSIGNED_CHAR)
+        array.SetName("colors")
+        sgrid.GetPointData().AddArray(array)
+    elif bands == 1:
+        arr = image.values
+        array = vn.numpy_to_vtk(arr.reshape(1,-1).T, deep=True, array_type=VTK_FLOAT)
+        array.SetName("band")
+        sgrid.GetPointData().AddArray(array)
+    else:
+        print ("Unsupported bands count (should be 1 or 3)", bands)
+
+    thresh = vtkThreshold()
+    thresh.SetInputData(sgrid)
+    thresh.SetInputArrayToProcess(0, 0, 0, vtkDataObject.FIELD_ASSOCIATION_POINTS, "z")
+    thresh.ThresholdBetween(-1e30, 1e30)
+    thresh.Update()
+
+#    return sgrid
+    return thresh.GetOutput()
+
--- NCube/NCubeImageOnTopographyBlockSource.py
+++ NCube/NCubeImageOnTopographyBlockSource.py
@@ -5,6 +5,7 @@
 # License: http://opensource.org/licenses/MIT
 
 from paraview.util.vtkAlgorithm import * 
+from NCube import _NCubeImageOnTopographyToGrid
 
 # load error fix for paraView 5.8.1rc1 Python3
 try:
@@ -12,69 +13,6 @@ try:
 except:
     import sys
     print (sys.exc_info()[0])
-
-def _NCubeImageOnTopographyToGrid(dem, image, mask_magic=False):
-    from vtk import vtkPoints, vtkStructuredGrid, vtkThreshold, vtkDataObject, VTK_FLOAT, VTK_UNSIGNED_CHAR
-    from vtk.util import numpy_support as vn
-    import numpy as np
-
-    # mask NaN areas
-    nanmask = (~np.any(np.isnan(image.values),axis=0)).astype(float)
-    # mask single channel zeroes if needed
-    if mask_magic:
-        # that's more correct way, only black pixels ignored
-        #zeromask = (~np.all(image.values==0,axis=0)).astype(float)
-        # that's magic for better borders
-        zeromask = (~np.any(image.values==0,axis=0)).astype(float)
-        mask = nanmask*zeromask
-    else:
-        mask = nanmask
-    mask[mask==0] = np.nan
-
-    xs = dem.x.values
-    ys = dem.y.values
-    values = mask * dem.values
-
-
-    # create raster mask by geometry and for NaNs
-    (yy,xx) = np.meshgrid(ys, xs)
-    vtk_points = vtkPoints()
-    points = np.column_stack((xx.ravel('F'),yy.ravel('F'),values.ravel('C')))
-    _points = vn.numpy_to_vtk(points, deep=True)
-    vtk_points.SetData(_points)
-
-    sgrid = vtkStructuredGrid()
-    sgrid.SetDimensions(len(xs), len(ys), 1)
-    sgrid.SetPoints(vtk_points)
-
-    array = vn.numpy_to_vtk(values.ravel(), deep=True, array_type=VTK_FLOAT)
-    array.SetName("z")
-    sgrid.GetPointData().AddArray(array)
-
-    bands = image.band.shape[0]
-    print ("bands", bands)
-    if bands == 3:
-        # RGB
-        colors = np.round(image.values)
-        array = vn.numpy_to_vtk(colors.reshape(3,-1).T, deep=True, array_type=VTK_UNSIGNED_CHAR)
-        array.SetName("colors")
-        sgrid.GetPointData().AddArray(array)
-    elif bands == 1:
-        arr = image.values
-        array = vn.numpy_to_vtk(arr.reshape(1,-1).T, deep=True, array_type=VTK_FLOAT)
-        array.SetName("band")
-        sgrid.GetPointData().AddArray(array)
-    else:
-        print ("Unsupported bands count (should be 1 or 3)", bands)
-
-    thresh = vtkThreshold()
-    thresh.SetInputData(sgrid)
-    thresh.SetInputArrayToProcess(0, 0, 0, vtkDataObject.FIELD_ASSOCIATION_POINTS, "z")
-    thresh.ThresholdBetween(-1e30, 1e30)
-    thresh.Update()
-
-#    return sgrid
-    return thresh.GetOutput()
 
 #------------------------------------------------------------------------------
 # N-Cube Image On Topography Source
